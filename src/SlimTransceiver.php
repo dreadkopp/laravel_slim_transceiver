1859d7a5f2ba30917ec6633778566873c55aa824
Arne Ziegert <arne.ziegert@gmx.de>
2021-01-12 19:44:56 +0100

    read/write from/to local session re-added

--- src/SlimTransceiver.php
+++ src/SlimTransceiver.php
@@ -9,29 +9,45 @@ use Slim\Http\Response;
 
 class SlimTransceiver
 {
-    private function dispatchToSlim(Request $request):Response
+    public function handle()
     {
-
-
-       if (session_status() !== PHP_SESSION_ACTIVE) {
-           session_set_save_handler(session()->getHandler(), true);
-           session_name(session()->getSessionConfig()['cookie']);
-           session_start();
-        }
-        return include public_path('sub_slim.php');
-
+        $this->createLocalSession();
+        $slim_response = $this->dispatchToSlim(\request());
+        $response = $this->translateSlimResponse($slim_response);
+        $this->gatherLocalSession();
+        
+        return $response;
+        
     }
-
+    
+    private function createLocalSession()
+    {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_set_save_handler(session()->getHandler(), true);
+            session_name(session()->getSessionConfig()['cookie']);
+            
+            session_id(session()->getId());
+            foreach (session()->all() as $key => $val) {
+                $_SESSION[$key] = $val;
+            }
+        }
+    }
+    
+    private function dispatchToSlim(Request $request): Response
+    {
+        return include public_path('sub_slim.php');
+    }
+    
     private function translateSlimResponse(Response $response)
     {
         $headers = $response->getHeaders();
         $body = $response->getBody();
         $code = $response->getStatusCode();
         $chunkSize = 4096;
-
+        
         $content = '';
         $json = false;
-
+        
         if ($body->isSeekable()) {
             $body->rewind();
         }
@@ -40,45 +56,45 @@ class SlimTransceiver
             $contentLength = $body->getSize();
         }
         $amountToRead = $contentLength;
-
+        
         while ($amountToRead > 0 && !$body->eof()) {
             $data = $body->read(min((int)$chunkSize, (int)$amountToRead));
             $content .= $data;
-
+            
             $amountToRead -= strlen($data);
-
+            
             if (connection_status() != CONNECTION_NORMAL) {
                 break;
             }
         }
-
+        
         if (isset($headers['Content-Type'])) {
-            if (in_array('application/json',$headers['Content-Type'])){
-                $content = json_decode($content,true);
+            if (in_array('application/json', $headers['Content-Type'])) {
+                $content = json_decode($content, true);
                 $json = true;
             }
         }
-
+        
         if (isset($headers['Location'][0])) {
             $location = $headers['Location'][0];
+            
             return \response()->redirectTo($location);
         }
-
-
+        
+        
         if ($json) {
             return \response()->json($content, $code);
         }
-
-        return \response($content,$code)->withHeaders($headers);
-
+        
+        return \response($content, $code)->withHeaders($headers);
+        
     }
-
-    public function handle()
+    
+    private function gatherLocalSession()
     {
-        $slim_response = $this->dispatchToSlim(\request());
-        $response = $this->translateSlimResponse($slim_response);
-
-        return $response;
-
+        foreach ($_SESSION as $key => $val) {
+            session()->put($key, $val);
+        }
+        session_unset();
     }
 }
